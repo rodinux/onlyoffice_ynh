f04fb226ef20ee83a1b228577de030417fd27cdc
Robles Rodolphe <rodolphe.robles@sfr.fr>
2020-04-29 15:35:53 +0200

    try to install document server from snapd packages

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,8 +5,8 @@
 #=================================================
 
 # dependencies used by the app
-pkg_dependencies="postgresql libstdc++6 redis-server rabbitmq-server libcurl4-nss-dev"
-
+#pkg_dependencies="postgresql libstdc++6 redis-server rabbitmq-server libcurl4-nss-dev"
+pkg_dependencies="snapd"
 #=================================================
 # PERSONAL HELPERS
 #=================================================
